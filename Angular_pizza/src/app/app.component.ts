90f4ef489b73632d911741fa27149838f70f62a4
lolewka <lolewka@gmail.com>
2025-07-31 16:17:02 +0300

    feat: implement loop for rendering HTML elements
    - Added *ngFor directive to display items
    - Created dynamic template for repeated elements
    - Implemented data binding for loop items

--- Angular_pizza/src/app/app.component.ts
+++ Angular_pizza/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular_pizza';
+export class AppComponent {// по факту отсюда нужна делать запрос на бэкенда но так как у нас его нет, мы сделаем так
+  public products = [
+    {
+      image: 'pizza-1.png',
+      title: 'Мясная Делюкс',
+      description: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы',
+    }, {
+      image: 'pizza-2.png',
+      title: 'Морская Премиум',
+      description: 'Перец, сыр, креветки, кальмары, мидии, лосось',
+    }, {
+      image: 'pizza-3.png',
+      title: 'Бекон и Сосиски',
+      description: 'Бекон, сыр, сосиски, ананас, томатная паста',
+    }, {
+      image: 'pizza-4.png',
+      title: 'Куриная Делюкс',
+      description: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста',
+    }, {
+      image: 'pizza-5.png',
+      title: 'Барбекю Премиум',
+      description: 'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили',
+    }, {
+      image: 'pizza-6.png',
+      title: 'Пепперони Дабл',
+      description: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная',
+    }, {
+      image: 'pizza-7.png',
+      title: 'Куриное трио',
+      description: 'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы',
+    }, {
+      image: 'pizza-8.png',
+      title: 'Сырная',
+      description: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный',
+    },
+  ]
+
+
 }
