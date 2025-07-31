ed253c1edeb6a266d8a2dcda4bb0aa4b8c7242c8
lolewka <lolewka@gmail.com>
2025-07-31 17:07:54 +0300

    feat: add header and footer components
    - Created separate HeaderComponent
    - Created separate FooterComponent
    - Added styles for both components
    - Implemented basic layout and functionality

--- Angular_pizza/src/app/app.module.ts
+++ Angular_pizza/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeaderComponent} from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +19,7 @@ import {FormsModule} from "@angular/forms";
     FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent, FooterComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
