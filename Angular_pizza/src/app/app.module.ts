da6965adf915c800fa2224c9707729c7ae0706fe
lolewka <lolewka@gmail.com>
2025-08-02 19:07:45 +0300

    copy product new component

--- Angular_pizza/src/app/app.module.ts
+++ Angular_pizza/src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HeaderComponent} from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
