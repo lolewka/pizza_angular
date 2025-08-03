0f8c70975d6283423e353be7c73db63414a8de8a
lolewka <lolewka@gmail.com>
2025-08-03 19:56:13 +0300

    added directives render element

--- Angular_pizza/src/app/directivs/cool-input.directive.ts
+++ Angular_pizza/src/app/directivs/cool-input.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appCoolInput]'
+  selector: '[collInput]'
 })
-export class CoolInputDirective {
+export class CoolInputDirective implements OnInit {
 
-  constructor() { }
+
+  constructor(private el: ElementRef,
+              private rend: Renderer2) {
+  }
+
+  ngOnInit() {
+    this.rend.setStyle(this.el.nativeElement, 'background-color', 'yellow  ');
+    this.rend.setAttribute(this.el.nativeElement, 'placeholder', this.el.nativeElement.getAttribute('placeholder') + '*');
+
+    const text = this.rend.createElement('span');
+    this.rend.setProperty(text, 'innerText', '*Обязательно к заполнению');
+    this.rend.setStyle(text, 'color', 'red');
+    this.rend.insertBefore(this.el.nativeElement.parentElement, text ,this.rend.nextSibling(this.el.nativeElement));
+  }
 
 }
