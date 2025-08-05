ae60a6fa2868efbca0187e0845708205c6742b42
lolewka <lolewka@gmail.com>
2025-08-05 17:38:52 +0300

    added structured directive or product component.html

---
+++ Angular_pizza/src/app/directivs/is-chiken.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+
+@Directive({
+  selector: '[isChiken]'
+})
+export class IsChikenDirective implements OnInit {
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+  ) {
+  }
+
+  @Input()
+  isChiken: string = '';
+
+  ngOnInit() {
+    if (this.isChiken.toLowerCase().includes('кур')) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+
+}
