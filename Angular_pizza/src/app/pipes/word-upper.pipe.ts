f6b689f7f86fdfa71c7b9b944be7dcefb6d0861d
lolewka <lolewka@gmail.com>
2025-08-05 18:39:49 +0300

    added word-upper array

--- Angular_pizza/src/app/pipes/word-upper.pipe.ts
+++ Angular_pizza/src/app/pipes/word-upper.pipe.ts
@@ -1,12 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'wordUpper'
 })
 export class WordUpperPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: string, wordPart: string[],): string {
+    let result = value;
 
+    wordPart.forEach(item => {
+      result = result.replace(new RegExp('[А-Яа-я]*' + item + '[а-я]*', 'g'), (match: string) => {
+        return match.toUpperCase();
+      });
+    });
+    return result;
+  }
 }
