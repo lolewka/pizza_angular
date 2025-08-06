1bfa89fa4e7bb813a7c419eac86afee48d7ba3b6
lolewka <lolewka@gmail.com>
2025-08-06 14:25:03 +0300

    cartService injected to footer.component.ts and header.component

--- Angular_pizza/src/app/components/footer/footer.component.ts
+++ Angular_pizza/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'footer-component',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cartService: CartService) { }
 
   ngOnInit(): void {
   }
--- Angular_pizza/src/app/components/header/header.component.ts
+++ Angular_pizza/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'header-component',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  constructor(public cartService: CartService) {
   }
 
   ngOnInit(): void {
