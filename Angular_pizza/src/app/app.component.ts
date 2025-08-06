ad29f4744b0390f4cca8ca77c78c9e04cfd38b28
lolewka <lolewka@gmail.com>
2025-08-06 14:35:21 +0300

    add cartProduct service

--- Angular_pizza/src/app/app.component.ts
+++ Angular_pizza/src/app/app.component.ts
@@ -1,53 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductType} from "./types/product.type";
+import {ProductService} from "./services/product.service";
+import {CartService} from "./services/cart.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {// по факту отсюда нужна делать запрос на бэкенда но так как у нас его нет, мы сделаем так
-  public products: ProductType[] = [
-    {
-      image: 'pizza-1.png',
-      title: 'Мясная Делюкс',
-      description: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы',
-    }, {
-      image: 'pizza-2.png',
-      title: 'Морская Премиум',
-      description: 'Перец, сыр, креветки, кальмары, мидии, лосось',
-    }, {
-      image: 'pizza-3.png',
-      title: 'Бекон и Сосиски',
-      description: 'Бекон, сыр, сосиски, ананас, томатная паста',
-    }, {
-      image: 'pizza-4.png',
-      title: 'Куриная Делюкс',
-      description: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста',
-    }, {
-      image: 'pizza-5.png',
-      title: 'Барбекю Премиум',
-      description: 'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили',
-    }, {
-      image: 'pizza-6.png',
-      title: 'Пепперони Дабл',
-      description: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная',
-    }, {
-      image: 'pizza-7.png',
-      title: 'Куриное трио',
-      description: 'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы',
-    }, {
-      image: 'pizza-8.png',
-      title: 'Сырная',
-      description: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный',
-    },
-  ];
+export class AppComponent implements OnInit {
+  // по факту отсюда нужна делать запрос на бэкенда но так как у нас его нет, мы сделаем так
+  public products: ProductType[] = [];
 
   //Объект который хранинит данные с наших импутов
   public formValues = {
     productTitle: '',
     address: '',
     phone: '',
+  }
+
+  constructor(private productService: ProductService,
+              public cartService: CartService,) {
+  }
+
+  ngOnInit() {
+    this.products = this.productService.getProducts()
   }
 
   //Функция которая отвечает за скролинг к определенному компоненту
@@ -59,6 +37,7 @@ export class AppComponent {// по факту отсюда нужна делат
   public addToCart(product: ProductType, target: HTMLElement): void {
     this.scrollTo(target);
     this.formValues.productTitle = product.title;
+    this.cartService.count++
   }
 
 //Валидация формы
@@ -79,7 +58,7 @@ export class AppComponent {// по факту отсюда нужна делат
     alert('Спасибо за заказ! Ваш заказ будет доставлен в течении 24 часов.');
 
     //что бы поля стирались преопределим объект
-   this.formValues = {
+    this.formValues = {
       productTitle: '',
       address: '',
       phone: '',
