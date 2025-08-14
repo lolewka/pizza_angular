4b94d076966aab7a4971d736b595bf4b92974f8d
lolewka <lolewka@gmail.com>
2025-08-14 18:02:47 +0300

    add observable and subscribe two

--- Angular_pizza/src/app/components/pages/main/main.component.ts
+++ Angular_pizza/src/app/components/pages/main/main.component.ts
@@ -9,20 +9,30 @@ import {Observable} from "rxjs";
 export class MainComponent implements OnInit {
 
   private observable: Observable<string>;
-
+  // private promise: Promise<string>;
   constructor() {
+    // this.promise = new Promise<string>((resolve) => {
+    //   setTimeout(() => {
+    //     resolve('hello');
+    //   }, 2000);
+    //
+    // });
     this.observable = new Observable((observer) => {
       setTimeout(() => {
         observer.next('hello');
       }, 2000)
+      setTimeout(() => {
+        observer.next('world');
+      }, 3000)
     })
-
   }
 
   ngOnInit(): void {
     this.observable.subscribe((param: string) => {
       console.log(param);
     })
+    // this.promise.then((param: string) => {
+    //   console.log(param);
+    // })
   }
-
 }
